37c74b4ba5dfe98311255c9957271200c96a727d
KTDR <33167082+KTDR@users.noreply.github.com>
2021-07-18 21:18:37 -0400

    Added function to get pokemon info by name to common.js

---
+++ src/common.js
@@ -0,0 +1,35 @@
+
+
+function getPokemonByNameSync(name) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", 'functions.php', false);
+    var param = JSON.stringify({functionname: 'getPokemonByName', arguments: [name]});
+    
+    //Send the proper header information along with the request
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.send(param);
+    
+    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+        var responseObject = JSON.parse(xhr.response).result;
+        return responseObject;
+    }
+}
+
+
+//incomplete, do not use
+function getPokemonByNameAsync(name) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", 'functions.php', true);
+    var param = JSON.stringify({functionname: 'getPokemonByName', arguments: [name]});
+
+    //Send the proper header information along with the request
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    xhr.onreadystatechange = function() { // Call a function when the state changes.
+        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+            var responseObject = JSON.parse(xhr.response).result;
+            return responseObject;
+        }
+    }
+    xhr.send(param);
+}
---
+++ src/functions.php
@@ -0,0 +1,61 @@
+<?php
+// https://stackoverflow.com/questions/8599595/send-json-data-from-javascript-to-php
+// https://www.geeksforgeeks.org/how-to-send-a-json-object-to-a-server-using-javascript/
+header('Content-Type: application/json');
+
+$data = json_decode(file_get_contents('php://input'));
+$aResult = (object)[];
+
+if( !isset($data->functionname) ) { $aResult->error = 'No function name!'; }
+
+if( !isset($data->arguments) ) { $aResult->error = 'No function arguments!'; }
+
+if( !isset($aResult->error) ) {
+
+    switch($data->functionname) {
+        case 'add':
+            if( !is_array($data->arguments) || (count($data->arguments) < 2) ) {
+                $aResult->error = 'Error in arguments!';
+            }
+            else {
+                $aResult->result = (($data->arguments[0]) + ($data->arguments[1]));
+            }
+            break;
+        case 'getPokemonByName':
+            if( !is_array($data->arguments) || (count($data->arguments) < 1) ) {
+                $aResult->error = 'Error in arguments!';
+            }
+            else {
+                $aResult->result = getPokemonByName($data->arguments[0]);
+            }
+            break;
+
+        default:
+            $aResult->error = 'Not found function '.$data->functionname.'!';
+            break;
+    }
+
+}
+echo json_encode($aResult);
+
+
+
+function getPokemonByName($name) {
+    $conn = new mysqli("localhost", "root", "", "pokemon");
+    $sql = "SELECT * from pokedex WHERE (name = '$name')";
+    $result = $conn->query($sql);
+    $pokemon = mysqli_fetch_object($result);
+    //fix integers from database being cast into strings, might be a more efficient way to do this
+    $pokemon->id = (integer) $pokemon->id;
+    $pokemon->total = (integer) $pokemon->total;
+    $pokemon->hp = (integer) $pokemon->hp;
+    $pokemon->att = (integer) $pokemon->att;
+    $pokemon->def = (integer) $pokemon->def;
+    $pokemon->spatt = (integer) $pokemon->spatt;
+    $pokemon->speed = (integer) $pokemon->speed;
+    $pokemon->generation = (integer) $pokemon->generation;
+    $pokemon->legendary = (integer) $pokemon->legendary;
+    return $pokemon;
+}
+
+?>
